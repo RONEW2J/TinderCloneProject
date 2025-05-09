ff7ddd38bcb8e4ebc576dffa65b1f05c1bb01f72
Ibragimov Ikhtiyor <90097546+RONEW2J@users.noreply.github.com>
2025-05-09 18:47:40 +0600

    Delete PycharmProjects/TinderCloneProject directory

--- PycharmProjects/TinderCloneProject/apps/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-
-# Register your models here.
-# For a custom user model, you might want to customize the admin display
-admin.site.register(User, UserAdmin)
